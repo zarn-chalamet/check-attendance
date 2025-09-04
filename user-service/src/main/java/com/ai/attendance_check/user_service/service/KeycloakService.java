53e24cdc54537d015ad6bba52fc1f9c5d9b82b72
zarn_chalamet <6531503195@lamduan.mfu.ac.th>
2025-09-04 21:45:06 +0700

    create api gateway service

---
+++ user-service/src/main/java/com/ai/attendance_check/user_service/service/KeycloakService.java
@@ -0,0 +1,4 @@
+package com.ai.attendance_check.user_service.service;
+
+public interface KeycloakService {
+}
---
+++ user-service/src/main/java/com/ai/attendance_check/user_service/service/impl/KeycloakServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ai.attendance_check.user_service.service.impl;
+
+import com.ai.attendance_check.user_service.service.KeycloakService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KeycloakServiceImpl implements KeycloakService {
+}
