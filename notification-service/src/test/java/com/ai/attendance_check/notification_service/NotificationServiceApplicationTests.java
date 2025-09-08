c7b2810fe8fa6113c1d50b7901a2ce2f5f5bbb7d
zarn_chalamet <6531503195@lamduan.mfu.ac.th>
2025-09-08 17:06:05 +0700

    init notification-service

---
+++ notification-service/src/test/java/com/ai/attendance_check/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.attendance_check.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
